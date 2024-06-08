3ade8fbc4189da2f39b8f690316441f3000b49fe
LucasLvy <lucaslevy10@gmail.com>
2024-06-08 17:40:58 +0200

    ready

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,16 +1,18 @@
-//! This crate provides Cairo language support for the [tree-sitter][] parsing library.
+//! This crate provides a cairo grammar for the [tree-sitter][] parsing library.
 //!
-//! Typically, you will use the [language][language func] function to add this language to a
+//! Typically, you will use the [language][language func] function to add this grammar to a
 //! tree-sitter [Parser][], and then use the parser to parse some code:
 //!
 //! ```
+//! use tree_sitter::Parser;
+//!
 //! let code = r#"
+//! fn double(x: i32) -> i32 {
+//!     x * 2
+//! }
 //! "#;
-//! let mut parser = tree_sitter::Parser::new();
-//! let language = tree_sitter_cairo::LANGUAGE;
-//! parser
-//!     .set_language(&language.into())
-//!     .expect("Error loading Cairo parser");
+//! let mut parser = Parser::new();
+//! parser.set_language(&tree_sitter_cairo::language()).expect("Error loading cairo grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! assert!(!tree.root_node().has_error());
 //! ```
@@ -20,34 +22,40 @@
 //! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
 
-use tree_sitter_language::LanguageFn;
+use tree_sitter::Language;
 
 extern "C" {
-    fn tree_sitter_cairo() -> *const ();
+    fn tree_sitter_cairo() -> Language;
 }
 
-/// The tree-sitter [`LanguageFn`] for this grammar.
-pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_cairo) };
+/// Returns the tree-sitter [Language][] for this grammar.
+///
+/// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
+pub fn language() -> Language {
+    unsafe { tree_sitter_cairo() }
+}
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
 pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
-// NOTE: uncomment these to include any queries that this grammar contains:
+/// The syntax highlighting query for this language.
+pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
 
-// pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
-// pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
-// pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
+/// The injections query for this language.
+pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
+
+// /// The symbol tagging query for this language.
 // pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_can_load_grammar() {
+    fn can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(&super::LANGUAGE.into())
-            .expect("Error loading Cairo parser");
+            .set_language(&super::language())
+            .expect("Error loading cairo grammar");
     }
 }
